a2d9fed2aaa6ee432a1c9c4a1a4507a25dd38272
Paul Aldred-Bann <pcaldredbann@gmail.com>
2018-11-14 17:49:44 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+} from 'reactstrap';
+import './app.scss';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.toggleNavbar = this.toggleNavbar.bind(this);
+		this.state = {
+			collapsed: true
+		};
+	}
+
+	toggleNavbar() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Navbar color="light" light expand="md">
+					<NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                <div id="footer">
+                </div>
+			</div>
+		);
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/public/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+	entry: {
+		main: './src/index.js'
+	},
+
+    target: 'web',
+    
+    mode: 'development',
+
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: function() {
+								return [require('precss'), require('autoprefixer')];
+							}
+						}
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.svg$/,
+				loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.woff$/,
+				loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.woff2$/,
+				loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.[ot]tf$/,
+				loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.eot$/,
+				loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]'
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js',
+		pathinfo: false
+	},
+
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        }),
+		HTMLWebpackPluginConfig
+	],
+
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+		compress: true,
+		port: 9000,
+		open: true
+	}
+};
